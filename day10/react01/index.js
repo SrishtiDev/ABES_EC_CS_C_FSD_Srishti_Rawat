ecaf3fefb7eb2c6154249fae3ff299c97fa0c113
Srishti Rawat <rawat.srishti1010@gmail.com>
2025-10-30 12:47:21 +0530

    commit

---
+++ day10/react01/index.js
@@ -0,0 +1,8 @@
+<div class="outer">
+    <div class="inner">
+        <div class="span-container">
+            <span>First Span</span>
+            <span>Second Span</span>
+        </div>
+    </div>
+</div>
